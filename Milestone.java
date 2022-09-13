8c7fd57bb9ee5f2dec124473247e41d9a377b733
unknown <chaithra.xworkz@gmail.com>
2022-09-13 11:03:06 +0530

    hello

---
+++ Milestone.java
@@ -0,0 +1,82 @@
+class Milestone
+{
+	static void showTheResponsibility()
+	{
+		System.out.println("start to showTheResponsibility");
+		  int age=18;
+		  char gender='F';
+		if(age==10)
+		{
+			System.out.println("showTheResponsibility of  school");
+			return;
+		}
+		if(age==16)
+		{
+			System.out.println("showTheResponsibility of  College");
+		}
+		/*if(age==18 && gender=='F')																								
+		{
+			System.out.println("showTheResponsibility of  Voter ID");
+			System.out.println("showTheResponsibility of  DL");
+			System.out.println("The girls are legally Married");
+			
+		}*/
+		else
+		{
+			System.out.println("The boys not legally Married");
+			return;
+		}
+		if(age==21)
+		{
+			System.out.println("The boys are getting juice");
+			System.out.println("showTheResponsibility of  Marriage");
+			return;
+		}
+		if(age==22 || age==23)
+		{
+			System.out.println("showTheResponsibility of  completing the Graduation");
+			return;
+		}
+		if(age==23 || age==25)
+		{
+			System.out.println("showTheResponsibility of  getting a job");
+			return;
+		}
+		if(age==28)
+		{
+			System.out.println("showTheResponsibility of  Kids");
+			return;
+		}
+		if(age==30)
+		{
+			System.out.println("showTheResponsibility of  having another kid");
+			return;
+		}
+		if(age==45)
+		{
+			System.out.println("showTheResponsibility of  kids Education");
+			return;
+		}
+		if(age==50)
+		{
+			System.out.println("showTheResponsibility of  kids Future");
+			return;
+		}
+		if(age==58)
+		{
+			System.out.println("Its time to Retired");
+			return;
+		}
+		if(age==60)
+		{
+			System.out.println("showTheResponsibility of  Grandkids");
+			return;
+		}
+		if(age==70)
+		{
+			System.out.println("The person is RIP");
+			return;
+		}
+		System.out.println("Exit to showTheResponsibility");
+	}   
+}
