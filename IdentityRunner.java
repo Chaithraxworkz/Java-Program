2b67857f3d94c7fb6ea4b5b994abe37de9ffa972
unknown <chaithra.xworkz@gmail.com>
2022-09-14 21:28:26 +0530

    string

---
+++ IdentityRunner.java
@@ -0,0 +1,68 @@
+class IdentityRunner
+{
+	public static void main(String[] args)
+	{
+		
+		Identity.printName("Chaithra");
+		Identity.printName("Srusti");
+		Identity.printName("Pavan");
+		Identity.printName("Rashmi");
+		Identity.printName("Surj");
+		Identity.printName("Nandani");
+		Identity.printName("Prerana");
+		Identity.printName("Arun");
+		Identity.printName("Nimmi");
+		Identity.printName("Pushpa");
+		Identity.printName("Bhavna");
+		
+		
+		Identity.printNameAndEmail("Chaithra","chaithram.xworksz@gmail.com");
+		Identity.printNameAndEmail("Sachin H L","sachinhl.xworkz@gmail.com");
+		Identity.printNameAndEmail("Raviteja k","raviteja.xworkz@gmail.com");
+		Identity.printNameAndEmail("Swati Kandakoor ","swatik.xworkz@gmail.com");
+		Identity.printNameAndEmail("Jaideep P","jaideepp.xworkz@gmail.com");
+		Identity.printNameAndEmail("B.V.Arun","arunbv.xworkz@gmail.com");
+		Identity.printNameAndEmail("fazlam ","fazlam.xworkz@gmai.com");
+		Identity.printNameAndEmail("Anusha KM","anushakm.xworkz@gmail.com");
+		Identity.printNameAndEmail("veena.p","veenapagati.xworkz@gmail.com");
+		Identity.printNameAndEmail("Nandini N","nandinin.xworkz@gmail.com");
+		Identity.printNameAndEmail("Niranjan C M","niranjan1xworkz@gmail.com");
+		Identity.printNameAndEmail("Chetan S V","chetasv.xworkz@gmail.com");
+		
+		Identity.printNameAndMobile("Chaithray",7996509441L);
+		Identity.printNameAndMobile("Sachin H L",9976249810L);
+		Identity.printNameAndMobile("Raviteja k",9980320520L);
+		Identity.printNameAndMobile("Jaideep P",7449821466L);
+		Identity.printNameAndMobile("B.V.Arun",99800557760L);
+		Identity.printNameAndMobile("Swati Kandakoor",7224206071L);
+		Identity.printNameAndMobile("fazlam ",9050607725L); 
+		Identity.printNameAndMobile("Niranjan C M",914665793L);
+		Identity.printNameAndMobile("Chetan S V",8250601942L);
+		Identity.printNameAndMobile("veena.p",7669143082L);
+		Identity.printNameAndMobile("Anusha KM",924876018L);
+		Identity.printNameAndMobile("Akshay",966498037L);
+		
+		Identity.printNameAndAge("Akshay",12);
+		Identity.printNameAndAge("Chaithray",22);
+		Identity.printNameAndAge("Sachin H L",24);
+		Identity.printNameAndAge("Jaideep P",25);
+		Identity.printNameAndAge("Swati Kandakoor",20);
+		Identity.printNameAndAge("B.V.Arun",30);
+		Identity.printNameAndAge("Raviteja k",35);
+		Identity.printNameAndAge("fazlam",31);
+		Identity.printNameAndAge("Chetan S V",34);
+		Identity.printNameAndAge("Niranjan C M",37);
+		Identity.printNameAndAge("Anusha KM",40);
+		Identity.printNameAndAge("Nandini N",19);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
