30b59ea8f18b01ed42f56e35bec5d688960cc4b1
Chaithraxworkz <113224711+Chaithraxworkz@users.noreply.github.com>
2022-09-22 07:11:44 +0530

    static array

--- GroceryStoreRunner.java
+++ GroceryStoreRunner.java
@@ -7,9 +7,9 @@ class GroceryStoreRunner
 		GroceryStore.setShoapName("Shankar Stores");
 		GroceryStore.setLocation("Arasikere");
 		GroceryStore.setItem("Rice");
-		GroceryStore.setPrice(1050L);
+		GroceryStore.setPrice(50);
 		GroceryStore.setType("Provision");
-		GroceryStore.setSize("25kg");
+		GroceryStore.setSize(25d);
 		GroceryStore.setHomeDelivery(true);
 		GroceryStore.setSelfService(true);
 		GroceryStore.setNoOfStaff(2);
@@ -27,28 +27,28 @@ class GroceryStoreRunner
 		String ref2=GroceryStore.getItem();
 		System.out.println(ref2);
 		
-		String ref3=GroceryStore.getPrice();
+		int ref3=GroceryStore.getPrice();
 		System.out.println(ref3);
 		
 		String ref4=GroceryStore.getType();
 		System.out.println(ref4);
 		
-		String ref5=GroceryStore.getSize();
+		double ref5=GroceryStore.getSize();
 		System.out.println(ref5);
 		
-		String ref6=GroceryStore.getHomeDelivery();
+		boolean ref6=GroceryStore.getHomeDelivery();
 		System.out.println(ref6);
 		
-		String ref7=GroceryStore.getSelfService();
+		boolean ref7=GroceryStore.getSelfService();
 		System.out.println(ref7);
 		
-		String ref8=GroceryStore.getNoOfStaff();
+		int ref8=GroceryStore.getNoOfStaff();
 		System.out.println(ref8);
 		
-		String ref9=GroceryStore.getRent();
+		double ref9=GroceryStore.getRent();
 		System.out.println(ref9);
 		
-		String ref10=GroceryStore.getBilling();
+		double ref10=GroceryStore.getBilling();
 		System.out.println(ref10);
 		
 	
