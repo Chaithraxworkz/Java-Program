e9749ac59e2dd78e796a1aa9ec190e7f55e03eac
Chaithraxworkz <113224711+Chaithraxworkz@users.noreply.github.com>
2022-09-23 06:44:12 +0530

    Wrapper class

---
+++ River.java
@@ -0,0 +1,42 @@
+class River 
+{
+	public static void main(String[] rivers)
+{
+        System.out.println("Entering main");
+		System.out.println("rivers :"+ rivers.length);
+        
+        String ref=rivers[0];
+        System.out.println("index 0 : "+ref);
+        
+        String ref1=rivers[1];
+        System.out.println("index 1 :"+ref1);
+		
+		String ref2=rivers[2];
+        System.out.println("index 2 : "+ref2);
+        
+        String ref3=rivers[3];
+        System.out.println("index 3 :"+ref3);
+		
+		String ref4=rivers[4];
+        System.out.println("index 4 : "+ref4);
+        
+        String ref5=rivers[5];
+        System.out.println("index 5 :"+ref5);
+		
+		String ref6=rivers[6];
+        System.out.println("index 6 : "+ref6);
+        
+        String ref7=rivers[7];
+        System.out.println("index 7 :"+ref7);
+		
+		String ref8=rivers[8];
+        System.out.println("index 8 : "+ref8);
+        
+        String ref9=rivers[9];
+        System.out.println("index 9 :"+ref9);
+
+		System.out.println("Close the main");
+        
+}
+
+}
